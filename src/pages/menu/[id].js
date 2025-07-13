1eda98e11138b0408d2ed329849155c529eb935b
Masih DR <masih.do2015@gmail.com>
2025-07-13 18:36:07 +0330

    change url completly in menu id

--- src/pages/menu/[id].js
+++ src/pages/menu/[id].js
@@ -1,27 +1,27 @@
 import { useRouter } from "next/router";
 import DetailsPage from "../../../components/templates/DetailsPage";
 
+const BASE_URL = "https://test-server-seven-zeta.vercel.app";
+
 function Details({ data }) {
   const router = useRouter();
-
   if (router.isFallback) {
     return <h2>Loading ...</h2>;
   }
-
   return <DetailsPage {...data} />;
 }
 
 export default Details;
 
 export async function getStaticPaths() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data`);
+  const res = await fetch(`${BASE_URL}/data`);
   const json = await res.json();
   const data = json.slice(0, 10);
-
+  
   const paths = data.map((food) => ({
     params: { id: food.id.toString() },
   }));
-
+  
   return {
     paths,
     fallback: true,
@@ -32,17 +32,18 @@ export async function getStaticProps(context) {
   const {
     params: { id },
   } = context;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data/${id}`);
+  
+  const res = await fetch(`${BASE_URL}/data/${id}`);
   const data = await res.json();
-
+  
   if (!data.id) {
     return {
       notFound: true,
     };
   }
-
+  
   return {
     props: { data },
-    revalidate: +process.env.REVALIDATE,
+    revalidate: 3600,
   };
 }
