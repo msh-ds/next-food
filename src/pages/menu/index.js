f7f4b4869acfcefc8a6793a209babf6b00533426
Masih DR <masih.do2015@gmail.com>
2025-07-13 18:20:43 +0330

    first commit

---
+++ src/pages/menu/index.js
@@ -0,0 +1,21 @@
+import MenuPage from "../../../components/templates/MenuPage";
+
+function Menu({ data }) {
+  return (
+    <div>
+      <MenuPage data={data} />
+    </div>
+  );
+}
+
+export default Menu;
+
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.BASE_URL}/data`);
+  const data = await res.json();
+
+  return {
+    props: { data },
+    revalidate: +process.env.REVALIDATE, //seconds
+  };
+}
